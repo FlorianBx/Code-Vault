fee84bf67dfcd38a6876a94e7ba2caa7b720308c
BFlorian91 <b.florian91@icloud.com>
2023-12-09 01:18:52 +0100

    Feat: add customButton, add fix some styles in profilePage

--- src/components/ButtonCustom.vue
+++ src/components/ButtonCustom.vue
@@ -2,7 +2,7 @@
 import { PropType } from "vue";
 
 defineProps({
-  name: {
+  label: {
     type: String,
     default: "",
   },
@@ -10,14 +10,26 @@ defineProps({
     type: String as PropType<"button" | "submit" | "reset">,
     default: "button",
   },
+  btnStyle: {
+    type: String,
+    default: "primary",
+    validator: (value: string): boolean =>
+      ["primary", "secondary"].includes(value),
+  },
 });
 </script>
 
 <template>
   <button
     :type="type"
-    class="rounded-md bg-vue px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    :class="[
+      btnStyle === 'primary' ? 'shadow-neumorphic hover:bg-vue text-white' : '',
+      btnStyle === 'secondary'
+        ? 'shadow-inner-neumorphic hover:bg-white text-primary hover:text-black'
+        : '',
+      'w-28 rounded-md px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
+    ]"
   >
-    Save
+    <slot />
   </button>
 </template>
