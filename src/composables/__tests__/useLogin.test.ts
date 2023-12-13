278103e88a93ec032cf4053f16156b3944b878e3
BFlorian91 <b.florian91@icloud.com>
2023-12-14 00:34:31 +0100

    fix: add secret_token_google in CI + add new rules to prettier

--- src/composables/__tests__/useLogin.test.ts
+++ src/composables/__tests__/useLogin.test.ts
@@ -4,30 +4,30 @@ import { createPinia, setActivePinia } from "pinia";
 import { useLogin } from "../useLogin";
 
 async function createTestUser() {
-  try {
-    await admin.auth().createUser({
-      email: "test@test.com",
-      password: "123456",
-    });
-    console.log("Test user created");
-  } catch (error) {
-    console.log("Error creating user: ", error);
-  }
+	try {
+		await admin.auth().createUser({
+			email: "test@test.com",
+			password: "123456",
+		});
+		console.log("Test user created");
+	} catch (error) {
+		console.log("Error creating user: ", error);
+	}
 }
 
 createTestUser();
 
 describe("useLogin", () => {
-  beforeEach(() => {
-    setActivePinia(createPinia());
-  });
+	beforeEach(() => {
+		setActivePinia(createPinia());
+	});
 
-  test("loginWithEmailAndPassword", async () => {
-    const { loginWithEmailAndPassword, error } = useLogin();
-    const user = { email: "test@test.com", password: "123456" };
-    const result = await loginWithEmailAndPassword(user);
+	test("loginWithEmailAndPassword", async () => {
+		const { loginWithEmailAndPassword, error } = useLogin();
+		const user = { email: "test@test.com", password: "123456" };
+		const result = await loginWithEmailAndPassword(user);
 
-    expect(result).not.toBe(null);
-    expect(error.value).toBe(null);
-  });
+		expect(result).not.toBe(null);
+		expect(error.value).toBe(null);
+	});
 });
--- src/services/firebase/firebase.config.ts
+++ src/services/firebase/firebase.config.ts
@@ -7,17 +7,17 @@ import admin from "firebase-admin";
 import serviceAccount from "../../../serviceAccountKey.ts";
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+	credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+	apiKey: import.meta.env.VITE_API_KEY,
+	authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_APP_ID,
+	measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -26,6 +26,6 @@ getAnalytics(app);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
-  connectAuthEmulator(auth, "http://localhost:9099");
+	connectAuthEmulator(auth, "http://localhost:9099");
 }
 export default app;
