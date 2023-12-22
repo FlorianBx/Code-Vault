c3107a69c2b5c6ed943d7073888a991d1fdbb6b4
BFlorian91 <b.florian91@icloud.com>
2023-12-22 16:57:05 +0100

    Feat: continue delete modal

--- src/components/__tests__/DeleteModal.test.ts
+++ src/components/__tests__/DeleteModal.test.ts
@@ -1,10 +1,27 @@
-import { describe, expect, it } from "vitest";
-import { mount } from "@vue/test-utils";
-import DeleteModal from "../DeleteModal.vue";
+import { describe, expect, it, beforeEach } from "vitest";
+import { mount, VueWrapper } from "@vue/test-utils";
+import DeleteModal from "../Modals/DeleteModal.vue";
 
 describe("DeleteModal.vue", () => {
-	it.skip("should render the modal", () => {
-		const wrapper = mount(DeleteModal);
+	let wrapper: VueWrapper;
+
+	beforeEach(async (): Promise<void> => {
+		wrapper = mount(DeleteModal);
+	});
+
+	it("should render the modal", () => {
 		expect(wrapper.html()).toContain("Delete");
+	});
+
+	it("should emit an event when the delete button is clicked", async (): Promise<void> => {
+		const deleteBtn = wrapper.find('[data-testid="delete-btn"]');
+		await deleteBtn.trigger("click");
+		expect(wrapper.emitted()).toHaveProperty("delete");
+	});
+
+	it("should emit an event when the cancel button is clicked", async (): Promise<void> => {
+		const cancelBtn = wrapper.find('[data-testid="cancel-btn"]');
+		await cancelBtn.trigger("click");
+		expect(wrapper.emitted()).toHaveProperty("cancel");
 	});
 });
