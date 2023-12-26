1405fc01f90034316dc557788fb6767b01b78f03
BFlorian91 <b.florian91@icloud.com>
2023-12-26 11:01:22 +0100

    fix: styles

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -3,14 +3,14 @@ import { searchQuery } from "../composables/useFilter.ts";
 </script>
 
 <template>
-	<div class="mb-4 w-64 sm:w-full lg:max-w-3xl sm:max-w-xl sm:px-8">
+	<div class="mb-4 w-64 sm:w-full">
 		<label
 			for="search"
 			class="block text-sm font-medium leading-6 ml-1 text-white"
 		>
 			Search
 		</label>
-		<div class="mt-2">
+		<div class="mt-2 w-full">
 			<div class="flex rounded-md bg-transparent ring-0">
 				<input
 					id="search"
